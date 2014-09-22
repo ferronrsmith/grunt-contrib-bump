80aa7ebc8ef53aa1ccaeff2df375a32b308872d2
ferron <ferronrsmith@gmail.com>
2014-09-22 12:15:47 -0400

    added bump:git

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,39 +6,36 @@
  * Licensed under the MIT license.
  */
 
-module.exports = function(grunt) {
-  'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        'test/*.js',
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      },
-    },
-    bump: {
-      options: {
-        commit: false,
-        tag: false,
-        filepaths: ['tmp/package.json'],
-      },
-    },
-    nodeunit: ['test/*_test.js'],
-  });
+    require('load-grunt-tasks')(grunt);
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                'test/*.js',
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        bump: {
+            options: {
+                commit: false,
+                tag: false,
+                filepaths: ['tmp/package.json']
+            }
+        },
+        nodeunit: ['test/*_test.js']
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-internal');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'nodeunit', /*'build-contrib'*/]);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'nodeunit', /*'build-contrib'*/]);
 };
--- test/bump_test.js
+++ test/bump_test.js
@@ -7,74 +7,83 @@ var fixture = path.resolve(__dirname, 'fixtures', 'package.json');
 var tmp = path.resolve(__dirname, '..', 'tmp', 'package.json');
 
 function testbump(cmd, cb) {
-  grunt.util.spawn({
-    grunt: true,
-    args: [cmd, '--no-color'],
-  }, function(err, result) {
-    if (err) { throw new Error(err); }
-    // If verbose is set output the results from the spawned command for debugging
-    if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--verbose') !== -1) {
-      console.log(String(result));
-    }
-    cb(String(result));
-  });
+    grunt.util.spawn({
+        grunt: true,
+        args: [cmd, '--no-color']
+    }, function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        // If verbose is set output the results from the spawned command for debugging
+        if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--verbose') !== -1) {
+            console.log(String(result));
+        }
+        cb(String(result));
+    });
 }
 
 exports.bump = {
-  setUp: function(done) {
-    grunt.file.copy(fixture, tmp);
-    done();
-  },
-  tearDown: function(done) {
-    grunt.file.delete(tmp);
-    done();
-  },
-  major: function(test) {
-    test.expect(2);
-    testbump('bump:major', function(result) {
-      test.ok(result.indexOf('from 0.1.0 to 1.0.0') !== -1, 'Should have bumped major version.');
-      test.equal(grunt.file.readJSON(tmp).version, '1.0.0', 'Should have written the version to the file.');
-      test.done();
-    });
-  },
-  minor: function(test) {
-    test.expect(2);
-    testbump('bump:minor', function(result) {
-      test.ok(result.indexOf('from 0.1.0 to 0.2.0') !== -1, 'Should have bumped minor version.');
-      test.equal(grunt.file.readJSON(tmp).version, '0.2.0', 'Should have written the version to the file.');
-      test.done();
-    });
-  },
-  patch: function(test) {
-    test.expect(2);
-    testbump('bump:patch', function(result) {
-      test.ok(result.indexOf('from 0.1.0 to 0.1.1') !== -1, 'Should have bumped patch version.');
-      test.equal(grunt.file.readJSON(tmp).version, '0.1.1', 'Should have written the version to the file.');
-      test.done();
-    });
-  },
-  prerelease: function(test) {
-    test.expect(2);
-    testbump('bump:prerelease', function(result) {
-      test.ok(result.indexOf('from 0.1.0 to 0.1.1-0') !== -1, 'Should have bumped prerelease and patch version.');
-      test.equal(grunt.file.readJSON(tmp).version, '0.1.1-0', 'Should have written the version to the file.');
-      test.done();
-    });
-  },
-  noargs: function(test) {
-    test.expect(2);
+    setUp: function (done) {
+        grunt.file.copy(fixture, tmp);
+        done();
+    },
+    tearDown: function (done) {
+        grunt.file.delete(tmp);
+        done();
+    },
+    major: function(test) {
+        test.expect(2);
+        testbump('bump:major', function(result) {
+            test.ok(result.indexOf('from 0.1.0 to 1.0.0') !== -1, 'Should have bumped major version.');
+            test.equal(grunt.file.readJSON(tmp).version, '1.0.0', 'Should have written the version to the file.');
+            test.done();
+        });
+    },
+    minor: function(test) {
+        test.expect(2);
+        testbump('bump:minor', function(result) {
+            test.ok(result.indexOf('from 0.1.0 to 0.2.0') !== -1, 'Should have bumped minor version.');
+            test.equal(grunt.file.readJSON(tmp).version, '0.2.0', 'Should have written the version to the file.');
+            test.done();
+        });
+    },
+    patch: function(test) {
+        test.expect(2);
+        testbump('bump:patch', function(result) {
+            test.ok(result.indexOf('from 0.1.0 to 0.1.1') !== -1, 'Should have bumped patch version.');
+            test.equal(grunt.file.readJSON(tmp).version, '0.1.1', 'Should have written the version to the file.');
+            test.done();
+        });
+    },
+    prerelease: function(test) {
+        test.expect(2);
+        testbump('bump:prerelease', function(result) {
+            test.ok(result.indexOf('from 0.1.0 to 0.1.1-0') !== -1, 'Should have bumped prerelease and patch version.');
+            test.equal(grunt.file.readJSON(tmp).version, '0.1.1-0', 'Should have written the version to the file.');
+            test.done();
+        });
+    },
+    git: function (test) {
+        test.expect(1);
+        testbump('bump:git', function (result) {
+            test.ok(grunt.file.readJSON(tmp).version.search(/\d+(\.\d)+\+sha\.[\w]+/) !== -1, 'Should have bumped git version.');
+            test.done();
+        });
+    },
+    noargs: function (test) {
+        test.expect(2);
 
-    grunt.util.spawn({
-      grunt: true,
-      args: ['bump', '--no-color'],
-    }, function(err, result) {
-      test.notStrictEqual(err, null, 'Should points an error when bump is called without args');
-      test.equal(String(result), 
-        'Running "bump" task\n>> Error: no modes specified.\n>> Valid modes are: major, minor, patch, prerelease.'+
-        '\nWarning: Use valid modes (or unambiguous mode abbreviations). Use --force to continue.'+
-        '\n\nAborted due to warnings.', 'Should tell the args that bump should be called with'
-      );
-      test.done();
-    });
-  }
+        grunt.util.spawn({
+            grunt: true,
+            args: ['bump', '--no-color']
+        }, function (err, result) {
+            test.notStrictEqual(err, null, 'Should points an error when bump is called without args');
+            test.equal(String(result),
+                    'Running "bump" task\n>> Error: no modes specified.\n>> Valid modes are: major, minor, patch, prerelease, git.' +
+                    '\nWarning: Use valid modes (or unambiguous mode abbreviations). Use --force to continue.' +
+                    '\n\nAborted due to warnings.', 'Should tell the args that bump should be called with'
+            );
+            test.done();
+        });
+    }
 };
